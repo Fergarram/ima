d6343e640227e8843622927ea3e0b2c51a96981e
Fernando Garcia <fergarram@gmail.com>
2026-01-04 14:28:56 -0600

    First step towards making this a package

---
+++ types/ima.d.ts
@@ -0,0 +1,12 @@
+export function useStaticTags(): any;
+export function useTags(options: any): any;
+export function parseTagArgs(args: any): {
+    props: {};
+    children: any;
+    ref: any;
+    innerHTML: any;
+};
+export function tagGenerator(_: any, tag: any, options: any): (...args: any[]) => any;
+export function getFrameTime(): number;
+export function escapeHtml(value: any): any;
+export function buildAttributesHtml(props: any): string;
